c7583d9db6c59396107ab13f21a9b38691f81f1a
acbst0 <125031376+acbst0@users.noreply.github.com>
2023-11-11 16:02:15 +0300

    11.11.2023 || 4:03

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,16 +6,26 @@
 /*   By: abostano <abostano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:12:32 by abostano          #+#    #+#             */
-/*   Updated: 2023/11/09 15:07:14 by abostano         ###   ########.fr       */
+/*   Updated: 2023/11/11 13:53:00 by abostano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#define BUFFER_SIZE 256
+int	ft_read(int fd)
+{
+	char	*buf;
+	int	val;
+	
+	val = 0;
+	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	while (val > 0)
+	{
+		val += read(fd, buf, BUFFER_SIZE);
+	}
+}
 
 char	*get_next_line(int fd)
 {
 	char	*readable;
-	read(fd, readable, BUFFER_SIZE);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,17 @@
 /*   By: abostano <abostano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:13:33 by abostano          #+#    #+#             */
-/*   Updated: 2023/11/09 15:01:37 by abostano         ###   ########.fr       */
+/*   Updated: 2023/11/11 14:05:11 by abostano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef
+#  define BUFFER_SIZE 256
+
+# endif
 
 # include <unistd.h>
 # include <stdio.h>
